4816192b755214f2a0f5853069cb9f76d319fc82
Nikesh Shrestha <shresthanikesh1074@gmail.com>
2025-09-13 12:02:35 -0700

    public images and changs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://bosquesf.com',
+  output: 'static',
   integrations: [
     sitemap()
   ]
